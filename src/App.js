5c3f6c7fbb9eeefd872d72a19ff88d7b005e431f
ibnatiqbal <ibbieibnat@gmail.com>
2021-03-04 00:48:30 -0500

    added my personal image to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
+import Logo from './kitten.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={Logo} className="App-logo" alt="logo" />
         <p>
           My name is Ibnat Iqbal and I'm going to master React!
         </p>
